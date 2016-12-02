e4ca45bffebf83e230e1900f33dabb310fc18761
Elias Meire <eliasmeire.dbz@gmail.com>
2016-12-02 22:23:50 +0100

    handle promise util

--- utils/promise.js
+++ utils/promise.js
@@ -1,11 +1,9 @@
-modules.exports = {
-  handlePromise: (promise, res) => {
-    promise
-      .then(colls => {
-        res.json(colls)
-      })
-      .catch((err) => {
-        res.send(err)
-      })
-  }
+module.exports = (promise, res) => {
+  promise
+    .then(colls => {
+      res.json(colls)
+    })
+    .catch((err) => {
+      res.send(err)
+    })
 }
