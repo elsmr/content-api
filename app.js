af3f7571bbc54004b3f61b108cdb205371f87795
Elias Meire <me@eliasmei.re>
2016-12-20 20:03:08 +0100

    Use helmet

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 const app = express()
+
+app.use(helmet())
 
 app.use(bodyParser.json())
 
