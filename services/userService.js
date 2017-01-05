b739f4679ffb304777d15828401d6fbecf22b679
Elias Meire <me@eliasmei.re>
2017-01-05 18:14:01 +0100

    no cache

--- services/userService.js
+++ services/userService.js
@@ -12,7 +12,6 @@ module.exports = {
     return new Promise((resolve, reject) => {
       db.get().collection('users').findOne({username})
         .then((docs) => {
-          console.log(docs)
           let err = error('Authentication failed', 400)
           if(!docs) {
             reject(err)
